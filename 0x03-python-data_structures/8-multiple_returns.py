d891b2772193ea3fd18d3b58e5bb9f1f9a9ee5d7
Iadolphe007 <iadolphe007@gmail.com>
2023-06-11 14:54:39 +0000

    len tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        return (0, None)
     else:
         i = len(sentence)
         fst_char = sentence[0]
