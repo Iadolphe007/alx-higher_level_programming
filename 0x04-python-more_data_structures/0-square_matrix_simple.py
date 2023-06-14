df5d4f5264ccd6e528b2e884755bd3f1b83061ac
Iadolphe007 <iadolphe007@gmail.com>
2023-06-14 13:47:38 +0000

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     result = []
 
     for row in matrix:
-        squared_matrix = list(map(lambda x: x ** 2 if isinstance(x, int) else x, row))
-        result.append(squared_matrix)
+        square = list(map(lambda x: x**2 if isinstance(x, int) else x, row))
+        result.append(square)
 
     return result
