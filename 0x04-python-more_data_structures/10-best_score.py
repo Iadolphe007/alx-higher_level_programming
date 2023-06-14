89c32d15548a715edc5d113a2e5ca8426ef45427
Iadolphe007 <iadolphe007@gmail.com>
2023-06-14 17:45:57 +0000

    max_value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-
-    max_key = None
-    max_value = float('-inf')
-
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_value = value
-            max_key = key
-        return max_key
+    max_value = max(a_dictionary, key=a_dictionary.get)
+    return max_value
