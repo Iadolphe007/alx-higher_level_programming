b6d45216a4d6b518d16f2755bc4d8b0736280bfc
Iadolphe007 <iadolphe007@gmail.com>
2023-06-26 20:19:25 +0000

    style

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import sys
+
 def safe_function(fct, *args):
+
     try:
         result = result = fct(*args)
         return result
-    except:
+
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
