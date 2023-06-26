a44ab1d0b40a09f1e5badf36cfd1a50c0a1f704b
Iadolphe007 <iadolphe007@gmail.com>
2023-06-26 19:08:32 +0000

    print int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for item in my_list[:x]:
-            try:
-                if isinstance(item, int):
-                print("{:d}".format(item), end=' ')
-                count += 1
-            except ValueError:
-                pass
-        print()
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i], end="")
+                    count +=1
+        except (ValueError, TypeError):
+            continue
+        print("")
         return count
-    except TypeError:
-        raise ValueError("Invalid argument: my_list must be a list")
+
 
