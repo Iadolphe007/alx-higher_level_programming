a4edc1364aad0687af911e80ad9940acabc802b1
Iadolphe007 <iadolphe007@gmail.com>
2023-06-26 18:24:24 +0000

    index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        for i in my_list:
-            new_list = my_list[i]
+        for i in range(x):
+            print(my_list[i], end=" ")
+            count += 1
         except IndexError: ('list index out of range')
-        except TypeError: ('list indices must be integers or slices, not float')
-        return new_list
+        return count
