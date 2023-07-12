610b47fb8e203c98ec52f0d35cc036189249fd30
Iadolphe007 <iadolphe007@gmail.com>
2023-07-12 21:43:49 +0000

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class taht inherit rectangle class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
