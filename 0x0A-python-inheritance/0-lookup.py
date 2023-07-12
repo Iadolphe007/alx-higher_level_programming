0e205ca180e753a509b2f0f7cc73753fa114e094
Iadolphe007 <iadolphe007@gmail.com>
2023-07-12 17:59:44 +0000

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This function retrun the list of
+available attribue of the object
+"""
+
+
+def lookup(obj):
+    """look for attributes and method"""
+    return dir(obj)
