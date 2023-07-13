2bca78fdb14b0b5c9c6592f95dbacb159725c87b
Iadolphe007 <iadolphe007@gmail.com>
2023-07-13 13:59:53 +0000

    open

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read file and print it"""
     with open(filename, "r", encoding="utf-8") as f:
         read_file = f.read()
-        print(read_file)
+        print("{}".format(read_file), end="")
