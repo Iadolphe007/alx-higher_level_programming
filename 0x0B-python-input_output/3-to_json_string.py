b6dd3bd72a986621567265f2f05b0e3a3eb86936
Iadolphe007 <iadolphe007@gmail.com>
2023-07-13 14:24:57 +0000

    seria

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""return to JSON"""
+
+
+def to_json_string(my_obj):
+    """
+    serialize the string with
+    JSON representation
+    """
+    return json.dumps(my_obj)
