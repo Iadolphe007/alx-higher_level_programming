c923d02cce967fb461a09817e197d2bb5bd22594
Iadolphe007 <iadolphe007@gmail.com>
2023-07-13 15:30:38 +0000

    dict

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""unction that returns the dictionary 
+description with simple data structure
+"""
+
+def class_to_json(obj):
+    """
+    unction that returns the dictionary
+    description with simple data structure
+    """
+    return obj.__dict__
