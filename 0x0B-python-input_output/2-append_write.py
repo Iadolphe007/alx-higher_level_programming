93d0ab30018297a2c1c84edc4e1936de5a44b491
Iadolphe007 <iadolphe007@gmail.com>
2023-07-13 14:16:18 +0000

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,9 @@
 
 
 def append_write(filename="", text=""):
+    """
+    append to the existing file
+    otherwise it create a new one
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
