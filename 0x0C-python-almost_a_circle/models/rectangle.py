9b1dd784916d7c7175dc01ce62233077e3ce115b
Iadolphe007 <iadolphe007@gmail.com>
2023-07-18 15:02:14 +0000

    excep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,8 @@ from models.base import Base
 class Rectangle(Base):
     """ class rectangle that inherity from base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
@@ -19,6 +19,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         self.__width = value
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
 
     @property
     def height(self):
@@ -27,14 +31,22 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         self.__height = value
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
 
     @property
     def x(self):
-        return self.__X
+        return self.__x
 
     @x.setter
     def x(self, value):
         self.__x = value
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
 
     @property
     def y(self):
@@ -43,3 +55,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
+        if type(value) != int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
