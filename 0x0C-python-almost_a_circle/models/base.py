483e16e4dbf8820621b5e135f72df9f14344293c
Iadolphe007 <iadolphe007@gmail.com>
2023-07-19 06:57:38 +0000

    to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """class base"""
+import json
 
 
 class Base:
@@ -12,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries == None or list_dictionaries == '[]':
+            return '[]'
+        else:
+           return json.dumps(list_dictionaries)
