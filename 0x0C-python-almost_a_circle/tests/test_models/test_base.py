215ff2b5203988395f75c09193c329289b0f4b65
Iadolphe007 <iadolphe007@gmail.com>
2023-07-19 16:46:15 +0000

    test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+import inspect
 import csv
 from models.base import Base
 from models.square import Square
@@ -79,28 +80,12 @@ class TestSquare(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """
-        Set up class method for the doc tests
-        """
         cls.setup = inspect.getmembers(Base, inspect.isfunction)
 
     def test_module_docstring(self):
-        """
-        Tests if module docstring documentation exist
-        """
         self.assertTrue(len(Base.__doc__) >= 1)
 
     def test_class_docstring(self):
-        """
-        Tests if class docstring documentation exist
-        """
         self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """
-        Tests if methods docstring documntation exist
-        """
-        for func in self.setup:
-            self.assertTrue(len(func[1].__doc__) >= 1)
 if __name__ == '__main__':
     unittest.main()
