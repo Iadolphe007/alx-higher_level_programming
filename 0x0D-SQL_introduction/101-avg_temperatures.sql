df18939af5f5d469ec97bc86374aed1a66fc2ea7
Iadolphe007 <iadolphe007@gmail.com>
2023-08-16 14:32:03 +0000

    average temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 --  script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(temp_fahrenheit) AS avg_temp FROM weather_data GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
