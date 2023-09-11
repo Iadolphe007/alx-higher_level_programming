6aeee28397742ad2240a5bdafb2bfb4031081d35
Iadolphe007 <iadolphe007@gmail.com>
2023-09-11 09:26:02 +0000

    add is

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 4) {
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
+}else if (process.argv.length === 3) {
+  console.log(process.argv[2] + ' ' + 'is' + ' ' +  process.argv[3]);
+} else if (process.argv.length === 2) {
+  console.log(process.argv[2] + ' ' + 'is' + ' '+ process.argv[2]);
+}
