b0f764ed4f2d34292528e2b6c102572ed7e5c491
Iadolphe007 <iadolphe007@gmail.com>
2023-09-11 14:31:02 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 const x = parseInt(process.argv[2]);
-if (!process.argv[2]) {
-  console.log('Missing size');
-} else if (isNaN(process.argv[2])) {
+if (isNaN(x) || x < 1) {
   console.log('Missing size');
 } else {
   for (let j = 0; j < x; j++) {
