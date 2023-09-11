9e71126af5305a9ba0910ab6718a4ddc143c6440
Iadolphe007 <iadolphe007@gmail.com>
2023-09-11 14:38:30 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,9 +5,9 @@ if (isNaN(x) || x < 1) {
   console.log('Missing size');
 } else {
   for (let j = 0; j < x; j++) {
-    let size = '';
+    let size = ' ';
     for (let i = 0; i < x; i++) {
-      size += 'x';
+      size += 'X';
     }
     console.log(size);
   }
