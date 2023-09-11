a79e5f6f4c0db37174ad4218fe6b0fc5d2805434
Iadolphe007 <iadolphe007@gmail.com>
2023-09-11 09:32:48 +0000

    fixed styling

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,8 @@
 
 if (process.argv.length === 4) {
   console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
-}else if (process.argv.length === 3) {
-  console.log(process.argv[2] + ' ' + 'is' + ' ' +  process.argv[3]);
+} else if (process.argv.length === 3) {
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
 } else if (process.argv.length === 2) {
-  console.log(process.argv[2] + ' ' + 'is' + ' '+ process.argv[2]);
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[2]);
 }
