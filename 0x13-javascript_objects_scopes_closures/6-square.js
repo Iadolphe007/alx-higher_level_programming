57114ed82eb1c5659b41142f1d1b714da1d2426f
Iadolphe007 <iadolphe007@gmail.com>
2023-09-12 14:59:44 +0000

    super extend

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,6 +3,11 @@
 const ParentSquare = require('./5-square');
 
 module.exports = class Square extends ParentSquare {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
