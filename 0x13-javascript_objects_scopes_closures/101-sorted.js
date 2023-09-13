e444897edb401d80124a76aa406739f86c43572d
Iadolphe007 <iadolphe007@gmail.com>
2023-09-13 08:51:40 +0000

    dict key value

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const newDict = {
-  1: [89, 91, 93],
-  2: [90, 94],
-  3: [92]
-};
+const newDict = {};
+
+for (const userId in dict) {
+  const ocurrenceCount = dict[userId];
+  if (ocurrenceCount in newDict) {
+    newDict[ocurrenceCount].push(userId);
+  } else {
+    newDict[ocurrenceCount] = [userId];
+  }
+}
 console.log(newDict);
