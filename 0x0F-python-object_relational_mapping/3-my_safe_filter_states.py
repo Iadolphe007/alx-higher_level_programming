7cf6ad381069f196052e15efcb18f4b43d9f2922
Iadolphe007 <iadolphe007@gmail.com>
2023-09-16 05:53:46 -0700

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, passwd=argv[2],
                          user=argv[1], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}'\
-            ORDER BY id ASC".format(argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s\
+            ORDER BY id ASC", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
