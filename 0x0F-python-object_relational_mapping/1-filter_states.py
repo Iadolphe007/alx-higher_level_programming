5eb132a5c0c463fffa8c0046c98a09d93adb9c3f
Iadolphe007 <iadolphe007@gmail.com>
2023-09-16 03:38:23 -0700

    sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,8 +3,10 @@
 Write a script that lists all states
 with a name starting with N
 '''
+
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          password=argv[2], db=argv[3], charset="utf8")
