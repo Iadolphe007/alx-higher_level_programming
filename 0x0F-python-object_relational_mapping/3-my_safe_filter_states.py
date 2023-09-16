fed012b36b80f86429fac6b987f78ce370b11a9f
Iadolphe007 <iadolphe007@gmail.com>
2023-09-16 05:45:35 -0700

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument. is safe from MySQL injections
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, passwd=argv[2],
+                         user=argv[1], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'\
+            ORDER BY id ASC".format(argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
