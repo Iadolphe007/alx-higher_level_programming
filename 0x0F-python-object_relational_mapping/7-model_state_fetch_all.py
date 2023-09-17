ec1dcce1d618ce6c469072aaa8ae82fe3d074bca
Iadolphe007 <iadolphe007@gmail.com>
2023-09-17 11:17:42 -0700

    sql

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql.+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                         argv[2],
                                                                         argv[3]))
     Base.metadata.create_all(engine)
