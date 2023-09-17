2302c170e2f685cfac92ed972c49cb107ad7dfc6
Iadolphe007 <iadolphe007@gmail.com>
2023-09-17 19:17:46 +0200

    sql

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of
+a State and an instance
+"""
+
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
