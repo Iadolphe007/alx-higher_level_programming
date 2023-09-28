cd9ec0b50ec80977a39d7d7a0aa8d41ba307ccf8
Iadolphe007 <iadolphe007@gmail.com>
2023-09-28 10:54:43 +0000

    fixing

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,10 +7,8 @@ def find_peak(list_of_integers):
     lst = list_of_integers
     if not lst:
         return None
-    #if lst[0] >= lst[1]:
-     #   return lst[0]
-    for i in range(len(lst) -1):
+    for i in range(len(lst) - 1):
         if lst[i] >= lst[i - 1] and lst[i] >= lst[i + 1]:
             return lst[i]
     if lst[i - 1] >= lst[i - 2]:
-        return lst[i -1]
+        return lst[i - 1]
