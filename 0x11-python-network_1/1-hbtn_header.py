01fd4033e25250b42568e614e500c8350d05bb30
Iadolphe007 <iadolphe007@gmail.com>
2023-09-30 14:57:59 +0000

    header value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""python script that takes in URL"""
+import sys
+import urllib.request
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    headers = response.info()
+    request_id = headers.get('X-Request-Id')
+    print(request_id)
