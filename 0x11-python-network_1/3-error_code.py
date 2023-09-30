70872f081e701d5e3a990a10305edf32800237e6
Iadolphe007 <iadolphe007@gmail.com>
2023-09-30 16:48:33 +0000

    handling error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request
+to the URL and displays the body of the response"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = file.read()
+            print(content)
+    except urllib.error.HTTPError as e:
+        print("Error code:{}".format(e.code))
