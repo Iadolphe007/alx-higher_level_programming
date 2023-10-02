548e95e32ebaad088457e4092c28cd6dc97c8602
Iadolphe007 <iadolphe007@gmail.com>
2023-10-02 18:09:04 +0000

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# fetches https://alx-intranet.hbtn.io/status
+import requests
+if __name__ == '__main__':
+    response = requests.get("https://intranet.hbtn.io/status")
+    text = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
