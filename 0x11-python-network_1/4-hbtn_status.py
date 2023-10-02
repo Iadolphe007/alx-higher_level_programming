0803b0026ab7affcedcc98f1869d1024abc3fe57
Iadolphe007 <iadolphe007@gmail.com>
2023-10-02 18:16:20 +0000

    fixing style

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-# fetches https://alx-intranet.hbtn.io/status
+""" fetches https://alx-intranet.hbtn.io/status"""
+
+
 import requests
+
+
 if __name__ == '__main__':
     response = requests.get("https://intranet.hbtn.io/status")
     text = response.text
