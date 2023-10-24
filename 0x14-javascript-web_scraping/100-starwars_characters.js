ed6c1c2dbee0cd710a825be659b3f802619aa0e8
Iadolphe007 <iadolphe007@gmail.com>
2023-10-24 09:26:11 +0000

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const mvId = peocess.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const mvId = process.argv[2];
+const url = `https://swapi.dev/api/films/${mvId}/`;
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    const data = JSON.parse(body);
-    const characters = data.characters;
-    for (const character of characters) {
-        request(character, (error, response, body) => {
-            if (error) {
-                console.log(error);
-            }
-            const characterData = JSON.parse(body);
-            console.log(characterData.name);
-        });
-    }
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  }
 });
