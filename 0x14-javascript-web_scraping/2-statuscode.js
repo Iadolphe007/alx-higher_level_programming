67076645edea5e409f3e58c7e4797fc67095adb6
Iadolphe007 <iadolphe007@gmail.com>
2023-10-24 10:00:20 +0200

    get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const require = require('request')
+httpUrl = process.argv[2]
+
+
+request.get(httpUrl, (error, response) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('code:', response.statusCode)
+    }
+
+})
